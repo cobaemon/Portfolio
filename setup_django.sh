524d114893f71f03f318dbab38779a5570a3d7c7
cobaemon <59654193+cobaemon@users.noreply.github.com>
2024-04-22 22:29:24 +0900

    デプロイのための修正
    ファイルパスの修正
    依存関係の修正

--- setup_django.sh
+++ setup_django.sh
@@ -2,7 +2,7 @@
 
 # 必要なパッケージの更新とインストール
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y python3 python3-pip python3-venv
+sudo apt install -y python3 python3-pip python3-venv gunicorn nginx
 
 # Django のインストール
 python3 -m pip install -r requirements.txt
