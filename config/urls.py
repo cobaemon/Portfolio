463486a1bb358e6616aab34b09bddb4b9bb56737
cobaemon <mgnco@outlook.jp>
2024-05-15 16:06:17 +0900

    Update urls.py
    Djangoのデフォルトのi18n URL設定をインクルード

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('portfolio/', include('portfolio.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
 
 urlpatterns += i18n_patterns(
