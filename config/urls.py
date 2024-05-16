4648c5a1eed77fd9c9c32fdb8f1fdab44d5d69c6
cobaemon <59654193+cobaemon@users.noreply.github.com>
2024-05-16 13:49:01 +0900

    テンプレートの編集
    webページのレイアウト変更
    言語選択機能の追加
    
    各サービスに適した生成画像を使用

--- config/urls.py
+++ config/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.i18n import i18n_patterns, set_language
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
+    path('set_language/', set_language, name='set_language')
 ]
 
 urlpatterns += i18n_patterns(
