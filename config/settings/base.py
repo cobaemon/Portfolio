5a4f3848d4b05efbc9d318c8dedd6be0fc996b89
cobaemon <59654193+cobaemon@users.noreply.github.com>
2024-05-17 16:02:12 +0900

    パフォーマンス向上のための修正
    nginxで静的ファイルを配信している場合whitenoiseは意味ないので削除

--- config/settings/base.py
+++ config/settings/base.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
@@ -138,8 +137,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
