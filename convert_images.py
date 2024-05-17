85aec3371cad1fa9cbdec716b72e8dcb757486d6
cobaemon <59654193+cobaemon@users.noreply.github.com>
2024-05-17 21:49:08 +0900

    パフォーマンス向上のための修正
    アバターの画像も高圧縮タイプを用意

--- convert_images.py
+++ convert_images.py
@@ -2,26 +2,26 @@ import os
 from PIL import Image
 import pillow_avif
 
-def convert_to_webp(png_path, webp_path):
-    with Image.open(png_path) as img:
+def convert_to_webp(image_path, webp_path):
+    with Image.open(image_path) as img:
         img.save(webp_path, 'webp')
 
-def convert_to_avif(png_path, avif_path):
-    with Image.open(png_path) as img:
+def convert_to_avif(image_path, avif_path):
+    with Image.open(image_path) as img:
         img.save(avif_path, 'avif')
 
 def convert_images(base_dir):
     for root, _, files in os.walk(base_dir):
         for file in files:
-            if file.lower().endswith('.png'):
-                png_path = os.path.join(root, file)
-                webp_path = os.path.splitext(png_path)[0] + '.webp'
-                avif_path = os.path.splitext(png_path)[0] + '.avif'
+            if file.lower().endswith(('.png', '.jpg', '.jpeg')):
+                image_path = os.path.join(root, file)
+                webp_path = os.path.splitext(image_path)[0] + '.webp'
+                avif_path = os.path.splitext(image_path)[0] + '.avif'
 
-                print(f'Converting {png_path} to {webp_path} and {avif_path}')
-                convert_to_webp(png_path, webp_path)
-                convert_to_avif(png_path, avif_path)
+                print(f'Converting {image_path} to {webp_path} and {avif_path}')
+                convert_to_webp(image_path, webp_path)
+                convert_to_avif(image_path, avif_path)
 
 if __name__ == "__main__":
-    base_dir = 'static'
+    base_dir = '.'
     convert_images(base_dir)
