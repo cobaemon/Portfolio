5006fa27fd6bc76110df5469fb263630aa810008
cobaemon <59654193+cobaemon@users.noreply.github.com>
2024-05-18 16:06:46 +0900

    セキュリティの最適化
    nonceを追加

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,19 +1,21 @@
+import os
+import base64
 from django.shortcuts import render
 from django.views.generic import FormView
 from django.http import HttpResponse
 from .forms import ContactForm
 
-# トップページ
 class Top(FormView):
     template_name = 'index.html'
     form_class = ContactForm
 
-    # フォームがバリデーションを通過した場合に呼ばれる
     def form_valid(self, form):
         form.send_email()
         return HttpResponse("Form submission successful")
-        
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        nonce = base64.b64encode(os.urandom(16)).decode('utf-8')
+        context['nonce'] = nonce
         context['form'] = self.form_class()
         return context
