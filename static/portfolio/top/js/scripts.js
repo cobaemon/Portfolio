1134adccb7ab00b430e30c5a425c8e88733b1fe6
cobaemon <59654193+cobaemon@users.noreply.github.com>
2024-05-20 10:53:43 +0900

    セキュリティの最適化

--- static/portfolio/top/js/scripts.js
+++ static/portfolio/top/js/scripts.js
@@ -136,39 +136,3 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
-
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
-const csrftoken = getCookie('csrftoken');
-
-fetch('https://portfolio.cobaemon.com/i18n/setlang/', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'X-CSRFToken': csrftoken
-    },
-    body: new URLSearchParams({
-        'language': 'ja'
-    }),
-    credentials: 'include'
-})
-.then(response => {
-    if (response.ok) {
-        console.log('Language set successfully');
-    } else {
-        console.error('Error setting language');
-    }
-});
