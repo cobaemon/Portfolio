aab4ef7de2e98e07d463129c06e533e8107da0d2
cobaemon <59654193+cobaemon@users.noreply.github.com>
2024-05-21 14:12:18 +0900

    言語切り替え機能の実装
    ページ上から対応済みの言語を選べるようにした
    対応済みは国連の公用語と日本語

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
 
 urlpatterns += i18n_patterns(
