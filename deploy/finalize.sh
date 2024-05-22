ce745a3aba887475b11247dfc081f4401d30e6fb
cobaemon <59654193+cobaemon@users.noreply.github.com>
2024-05-22 17:38:47 +0900

    cronjobの設定コマンドの修正
    複数回実行された場合に重複して登録されないように変更

--- deploy/finalize.sh
+++ deploy/finalize.sh
@@ -22,7 +22,8 @@ echo -e "${LIGHT_MAGENTA}Restarting Nginx...${RESET}"
 sudo systemctl restart nginx
 
 # Cronjobでパブリックアドレスの変更を自動でドメインに反映
-echo -e "${GREEN}Setting up cron job for updating Route 53 record...${RESET}"
-(crontab -l 2>/dev/null; echo "*/10 * * * * /home/cobalt/Portfolio/deploy/update_route53_record.sh >> /home/cobalt/Portfolio/log/update_route53_record.log 2>&1") | crontab -
+# Cronジョブの設定
+CRON_JOB="*/10 * * * * /home/cobalt/Portfolio/deploy/update_route53_record.sh >> /home/cobalt/Portfolio/log/update_route53_record.log 2>&1"
+(crontab -l 2>/dev/null | grep -F "$CRON_JOB" || (crontab -l 2>/dev/null; echo "$CRON_JOB")) | crontab -
 
 echo -e "${LIGHT_MAGENTA}Finalize completed successfully.${RESET}"
