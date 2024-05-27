8c2c9c360097cb54ec0a4924fa25b3647c87e6e5
cobaemon <59654193+cobaemon@users.noreply.github.com>
2024-05-27 22:47:59 +0900

    Nonceの設定

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
 from django.views.generic import FormView
 from django.http import HttpResponse
 from .forms import ContactForm
-from csp.decorators import csp_update
 
 class Top(FormView):
     template_name = 'index.html'
@@ -16,7 +14,3 @@ class Top(FormView):
         context = super().get_context_data(**kwargs)
         context['form'] = self.form_class()
         return context
-
-    @csp_update(SCRIPT_SRC="'self' 'nonce-{nonce}'", STYLE_SRC="'self' 'nonce-{nonce}'")
-    def dispatch(self, *args, **kwargs):
-        return super().dispatch(*args, **kwargs)
