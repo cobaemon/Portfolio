9642e3e0ffb2a224d08c9e7b99a8de88ceecac33
cobaemon <59654193+cobaemon@users.noreply.github.com>
2024-05-29 16:33:43 +0900

    nonceの設定

--- config/settings/base.py
+++ config/settings/base.py
@@ -66,7 +66,11 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'csp.context_processors.nonce',
             ],
+            'libraries':          {
+                'csp': 'csp.templatetags.csp',
+            }
         },
     },
 ]
