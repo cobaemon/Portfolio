7c7411dba19b4c2e7c486ca0366d8ffda26f71bb
cobaemon <59654193+cobaemon@users.noreply.github.com>
2024-06-05 15:28:28 +0900

    ネットワークの分離
    ホストとの通信用と、コンテナ間用のインターフェースを作成

--- deploy/iptables-setup.sh
+++ deploy/iptables-setup.sh
@@ -77,7 +77,6 @@ iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 # Dockerインターフェースへのトラフィックの許可
 iptables -A FORWARD -o docker0 -j DOCKER
 iptables -A FORWARD -i docker0 -j ACCEPT
-iptables -A FORWARD -o br-918b2a66b087 -j DOCKER
 
 # ループバックインターフェースのトラフィックを許可
 iptables -A INPUT -i lo -j ACCEPT
